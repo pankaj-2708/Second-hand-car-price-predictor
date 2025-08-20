da20821261202812bfcdeae2c222bd1900247450
pankaj-2708 <pankajmaulekhi32@gmail.com>
2025-08-20 17:07:07 +0530

    path configured

--- second_hand_car_price_prediction/bring_dataset.py
+++ second_hand_car_price_prediction/bring_dataset.py
@@ -2,9 +2,9 @@ import gdown
 import pathlib
     # Option 1: Using file ID
     
-output_path=pathlib("data")
+output_path=pathlib.Path("data")
 output_path=output_path / 'raw'
 output_path.mkdir(parents=True,exist_ok=True)
 
 file_id = '10XN9ugutKXcoUzZhqztRFsGNrwR_JlD8'
-gdown.download(id=file_id, output=output_path, quiet=False)
+gdown.download(id=file_id, output='./data/raw/car_details.csv', quiet=False)
